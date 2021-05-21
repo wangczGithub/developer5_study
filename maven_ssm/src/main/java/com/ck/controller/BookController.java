cdf7018bae8b201a499023f4d6618da2722518f6
zhongtt <2198885632@qq.com>
2021-05-21 11:19:43 +0800

    我不是老王

--- maven_ssm/src/main/java/com/ck/controller/BookController.java
+++ maven_ssm/src/main/java/com/ck/controller/BookController.java
@@ -26,6 +26,7 @@ public class BookController {
     @GetMapping("queryAll")
     public String queryAll(ModelMap map){
         System.out.println("我是老王");
+        System.out.println("我不是老王！");
         List<Book> books = bookService.queryAll();
         map.put("bookList", books);
         return "book";
